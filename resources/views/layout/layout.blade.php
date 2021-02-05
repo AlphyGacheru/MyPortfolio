229517d484e9dc9400648f0f5894d334ab5bc010
Alphy Gacheru <alphygacheru@gmail.com>
2021-02-06 02:25:14 +0300

    secure_asset added

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    @yield('title')
    
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
 
 </head>
 <body>
